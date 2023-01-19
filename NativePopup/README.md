5706fdae914c2b174bede952a8f2156f5d6563c0
sungwoo.yeo <yeosu0107@naver.com>
2023-01-19 12:40:32 +0900

    프로젝트 추가

---
+++ NativePopup/README.md
@@ -0,0 +1,3 @@
+# NativePopup
+
+A description of this package.
---
+++ NativePopup/Tests/NativePopupTests/NativePopupTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NativePopup
+
+final class NativePopupTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NativePopup().text, "Hello, World!")
+    }
+}
