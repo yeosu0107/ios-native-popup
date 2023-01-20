12e073a643dfa752a49d2a694adafc9ff72847c2
sungwoo.yeo <yeosu0107@naver.com>
2023-01-20 15:48:06 +0900

    rootviewcontroller를 조회하는 기능 추가, @cdecl() 빌드 오류 픽스

--- NativePopup/Sources/NativePopup/NativePopup.swift
+++ NativePopup/Sources/NativePopup/NativePopup.swift
@@ -2,6 +2,14 @@ import UIKit
 import SwiftUI
 import Foundation
 
+private func getRootViewController() -> UIViewController? {
+    //let window = UIWindow(frame: UIScreen.main.bounds)
+    //let rootViewController = window.rootViewController
+
+    let rootViewController = UIApplication.shared.windows.first?.rootViewController
+    
+    return rootViewController
+}
 
 private func showAlert(alertInfo: AlertInfo) -> some View {
     let alertView = AlertView()
@@ -10,13 +18,21 @@ private func showAlert(alertInfo: AlertInfo) -> some View {
     return alertView
 }
 
-//@_cdecl("justPrint")
-public func justPrint(message: String) {
-    print(message)
+@_cdecl("justPrint")
+public func justPrint(message: String) -> String {
+    let copyMsg = "justPrint: " + message
+    print(copyMsg)
+    return copyMsg
 }
 
-//@_cdecl("showAlert_oneBtn")
-public func showAlert(title: String, message: String, okBtn: String?) -> UIViewController {
+@_cdecl("showAlert_oneBtn")
+public func showAlertOneBtn(title: String, message: String, okBtn: String?) {
+    let rootViewController = getRootViewController()
+    if rootViewController == nil {
+        print("rootViewController is nil")
+        return
+    }
+    
     let alertView = showAlert(alertInfo: AlertInfo(alertType: .onebtn, title: title, message: message, okBtn: okBtn, cancelBtn: nil))
     let uiHostController = UIHostingController(rootView: alertView.edgesIgnoringSafeArea(.all))
     uiHostController.view.backgroundColor = .clear
@@ -25,11 +41,18 @@ public func showAlert(title: String, message: String, okBtn: String?) -> UIViewC
     NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "dismissModal"), object: nil, queue: nil) { [weak uiHostController] _ in
         uiHostController?.dismiss(animated: true, completion: nil)
     }
-    return uiHostController
+    
+    rootViewController?.present(uiHostController, animated: true)
 }
 
-//@_cdecl("showAlert_twoBtn")
-public func showAlert(title: String, message: String, okBtn: String?, cancelBtn: String?) -> UIViewController {
+@_cdecl("showAlert_twoBtn")
+public func showAlertTwoBtn(title: String, message: String, okBtn: String?, cancelBtn: String?) {
+    let rootViewController = getRootViewController()
+    if rootViewController == nil {
+        print("rootViewController is nil")
+        return
+    }
+    
     let alertView = showAlert(alertInfo: AlertInfo(alertType: .twobtn, title: title, message: message, okBtn: okBtn, cancelBtn: nil))
     let uiHostController = UIHostingController(rootView: alertView.edgesIgnoringSafeArea(.all))
     uiHostController.view.backgroundColor = .clear
@@ -38,7 +61,8 @@ public func showAlert(title: String, message: String, okBtn: String?, cancelBtn:
     NotificationCenter.default.addObserver(forName: Notification.Name(rawValue: "dismissModal"), object: nil, queue: nil) { [weak uiHostController] _ in
         uiHostController?.dismiss(animated: true, completion: nil)
     }
-    return uiHostController
+    
+    rootViewController?.present(uiHostController, animated: true)
 }
 
 
