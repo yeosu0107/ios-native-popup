58680b604d4f2a092bed5cbdf3ae816fcf49eb82
sungwoo.yeo <yeosu0107@naver.com>
2023-01-26 19:37:38 +0900

    테스트 코드 삭제

--- NativePopup/Tests/NativePopupTests/NativePopupTests.swift
+++ NativePopup/Tests/NativePopupTests/NativePopupTests.swift
@@ -2,11 +2,5 @@ import XCTest
 @testable import NativePopup
 
 final class NativePopupTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        let printValue = justPrint(message: "Hello, World!")
-        XCTAssertEqual(printValue, "justPrint: Hello, World!")
-    }
+
 }
