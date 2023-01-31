9cde4aaef2809678a44235e76c005c971d1cbc48
sungwoo.yeo <yeosu0107@naver.com>
2023-01-31 14:45:13 +0900

    빌드 시 BUILD_LIBRARY_FOR_DISTRIBUTION = YES 플래그 추가

--- xcodebuild-script.sh
+++ xcodebuild-script.sh
@@ -6,7 +6,7 @@ cd NativePopup
 swift package generate-xcodeproj --skip-extra-files
 
 # framework 빌드
-xcodebuild -project NativePopup.xcodeproj -scheme NativePopup-Package -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=.
+xcodebuild -project NativePopup.xcodeproj -scheme NativePopup-Package -configuration Release -sdk iphoneos CONFIGURATION_BUILD_DIR=. SKIP_INSTALL=NO BUILD_LIBRARY_FOR_DISTRIBUTION=YES
 
 rm -rf *.dSYM
 rm -rf *.xcodeproj
